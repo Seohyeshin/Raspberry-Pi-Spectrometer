1b142316f1893a1ceda9a9b2575605beccd6df22
21-hanini <79555926+21-hanini@users.noreply.github.com>
2022-03-15 19:30:20 +0900

    Add files via upload

---
+++ ini.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x = [300, 400, 500, 600]
+y = [100, 20, 300, 50]
+plt.plot(x, y)
+plt.show()
+plt.savefig('graph.jpg')
+#import pyautogui
+#pyautogui.screenshot('test1.png')
