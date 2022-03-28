e27a06d408ffda02bb13f52f62297f5fdd7ea0e9
21-hanini <79555926+21-hanini@users.noreply.github.com>
2022-03-28 22:46:37 +0900

    Add files via upload

--- ini.py
+++ ini.py
@@ -1,10 +1,30 @@
+#별도의 과정 없이 그냥 같이 쓰면 그래프가 합쳐져서 나옴
 import matplotlib.pyplot as plt
 
-x=list(range(200,800,100))
-y=[100, 400, 300, 500, 200, 70]
-a=list(range(200, 800, 100))
-b=[300, 400, 500, 650, 300, 300]
+x = [300, 400, 500, 600]
+y = [100, 20, 300, 50]
+a = list(range(200,800,100))
+b = [100, 20, 300, 50, 500, 300]
+
 plt.plot(x, y)
 plt.plot(a, b)
 
+n = len(y) - 2
+Y = []
+X = []
+for i in range(n) :
+    j= i+1
+    if y[j] != 0:
+        if (y[j]-y[j-1]) > 0 and (y[j]-y[j+1]) >0:
+            Y.append(y[j])
+            X.append(x[j])
+
+
+plt.scatter(X,Y)
+
+plt.savefig('graph.jpg')
 plt.show()
+
+
+
+
