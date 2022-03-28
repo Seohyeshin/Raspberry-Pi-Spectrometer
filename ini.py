a5fa4857242e7addb201470e48c936594cc030e3
21-hanini <79555926+21-hanini@users.noreply.github.com>
2022-03-28 22:41:45 +0900

    Add files via upload

--- ini.py
+++ ini.py
@@ -1,21 +1,10 @@
 import matplotlib.pyplot as plt
 
-x = list(range(200,800,100))
-y = [100, 20, 300, 50, 500, 300]
+x=list(range(200,800,100))
+y=[100, 400, 300, 500, 200, 70]
+a=list(range(200, 800, 100))
+b=[300, 400, 500, 650, 300, 300]
 plt.plot(x, y)
-n = len(y) - 2
-Y = []
-X = []
-for i in range(n) :
-    j= i+1
-    if y[j] != 0:
-        if (y[j]-y[j-1]) > 0 and (y[j]-y[j+1]) >0:
-            Y.append(y[j])
-            X.append(x[j])
+plt.plot(a, b)
 
-plt.scatter(X,Y)
-
-plt.savefig('graph.jpg')
 plt.show()
-#import pyautogui
-#pyautogui.screenshot('test1.png')
