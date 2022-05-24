318c5fe11508f7a6d577f34d687b8d1fc382cff8
arim-ryou <reach_p_0125@naver.com>
2022-05-24 18:51:36 +0900

    ui1

--- ui1.py
+++ ui1.py
@@ -34,7 +34,7 @@ def graph():
     ini.making()
     photo=PhotoImage(file='graph.png')
     list_file=Label(root,image=photo , width=630, height=450)
-    list_file.pack(side="top")
+    list_file.grid(row=1, column=0)
 
 root=Tk()
 root.title("spectrometer")
@@ -50,7 +50,7 @@ menu_file.add_command(label="끝내기", command=root.quit)
 menu.add_cascade(label="파일", menu=menu_file)
 
 file_frame=Frame(root)
-file_frame.pack(fill="x", padx=5, pady=3)
+file_frame.grid(row=0, column=0, ipadx=260, padx=5, pady=3)
 
 
 #저장하기 버튼
@@ -62,67 +62,66 @@ btn_save_file.pack(side="right")
 if os.path.isfile("graph.png"):
     photo=PhotoImage(file='graph.png')
     list_file=Label(root,image=photo , width=630, height=450)
-    list_file.pack(side="top")
+    list_file.grid(row=1, column=0)
 else: 
-    list_file=Label(root, text = "그래프가 없습니다.", bg="white", width=60, height=20)
-    list_file.pack(side="top")
+    list_file=Label(root, text = "그래프가 없습니다.", bg="white", width=90, height=30)
+    list_file.grid(row=1, column=0)
     
 #저장 경로
 path_frame=LabelFrame(root, text="불러오기")
-path_frame.pack(fill="x", padx=5, pady=3, ipady=3)
+path_frame.grid(row=2, column=0, padx=5, pady=3, ipady=3, ipadx=35)
 
 add_list=["1번","2번","3번"]
 cmb_add=ttk.Combobox(path_frame, state="readonly", values=add_list , width=60)
 cmb_add.current(0)
-cmb_add.pack(side="left", padx=5, pady=3)
+cmb_add.grid(row=0, column=0, padx= 5)
 
 btn_add_path=Button(path_frame, command=add ,text="찾아보기", width=12)
-btn_add_path.pack(side="right", padx=3, pady=1)
+btn_add_path.grid(row=0, column=1)
 
 
 #분석 프레임
 frame_option=LabelFrame(root, text="분석")
-frame_option.pack(padx=5, pady=3, ipady=3)
+frame_option.grid(row=3, column=0, pady=3, ipady=3, ipadx=50)
 
 #계산1
 lbl_width=Label(frame_option, text="파장:", width=8)
-lbl_width.pack(side="left", padx=1, pady=3)
+lbl_width.grid(row=0, column=0, padx=1, pady=3)
 
 opt_width=["원본유지"]
 cmb_width=ttk.Combobox(frame_option, state="readonly", values=opt_width, width=10)
 cmb_width.current(0)
-cmb_width.pack(side="left", padx=8, pady=3)
+cmb_width.grid(row=0, column=1, padx=8, pady=3)
 
 
 #계산2
 lbl_space=Label(frame_option, text="최고값:", width=8)
-lbl_space.pack(side="left", padx=1, pady=3)
+lbl_space.grid(row=0, column=2, padx=1, pady=3)
 
 
 opt_space=["원본유지"]
 cmb_space=ttk.Combobox(frame_option, state="readonly", values=opt_space, width=10)
 cmb_space.current(0)
-cmb_space.pack(side="left", padx=8, pady=3)
+cmb_space.grid(row=0, column=3, padx=8, pady=3)
 
 #계산3
 lbl_format=Label(frame_option, text="계산3:", width=8)
-lbl_format.pack(side="left", padx=1, pady=3)
+lbl_format.grid(row=0, column=4, padx=1, pady=3)
 
 opt_format=["원본유지"]
 cmb_format=ttk.Combobox(frame_option, state="readonly", values=opt_format, width=10)
 cmb_format.current(0)
-cmb_format.pack(side="left", padx=8, pady=3)
+cmb_format.grid(row=0, column=5, padx=8, pady=3)
 
 #실행 프레임
 frame_run=Frame(root)
-frame_run.pack(fill="x", padx=5, pady=3)
+frame_run.grid(row=4, column=0, ipadx=215, pady=3)
 
-btn_close=Button(frame_run, padx=5, pady=3, text="닫기", width=12, command=quit)
-btn_close.pack(side="right", padx=5, pady=3)
+btn_close=Button(frame_run, pady=3, text="닫기", width=12, command=quit)
+btn_close.pack(side="right")
 
-btn_start=Button(frame_run, padx=5, pady=3, text="시작", width=12)
-btn_start.pack(side="right", padx=5, pady=3)
-btn_start.config(command=graph)
+btn_start=Button(frame_run, pady=3, text="시작", width=12, command=graph)
+btn_start.pack(side="right")
 
 root.resizable(False, False)
 root.mainloop()
